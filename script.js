4194110ef2066b474752a880b023726cff272595
ValF456 <valentyn.fasolia@oa.edu.ua>
2025-05-05 21:10:36 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f4;
+    text-align: center;
+    padding: 30px;
+  }
+  
+  .container {
+    background: white;
+    padding: 20px;
+    border-radius: 10px;
+    display: inline-block;
+    box-shadow: 0 0 10px rgba(0,0,0,0.1);
+  }
+  
+  input {
+    padding: 10px;
+    width: 200px;
+    margin-right: 10px;
+  }
+  
+  button {
+    padding: 10px 15px;
+    cursor: pointer;
+  }
+  
+  li {
+    list-style-type: none;
+    margin: 10px 0;
+  }
+  
+  li button {
+    margin-left: 20px;
+    background-color: crimson;
+    color: white;
+    border: none;
+    padding: 5px 10px;
+    border-radius: 5px;
+  }
+  
